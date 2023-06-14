f5789e7b61d44d0bb1b941bebf44241270cd65a4
PIUS <relimoringa@gmail.com>
2023-06-14 22:31:47 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Code your solutions in this file
+for (let age = 3; age > 40; age++){
+
+    console.log(`im ${age} years old . Happy Birthday to me!`);
+
+
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+let names = ["Guadalupe", "Ollie", "Aki"];
+let event = "surprise";
+
+function writeCards(names, event) {
+  let thankYouMessages = []; 
+  for (let j = 0; j < names.length; j++) {
+    let message = `Thank you, ${names[j]}, for the wonderful ${event} gift!`;
+    console.log(message);
+    thankYouMessages.push(message); 
+  }
+
+  return thankYouMessages;
+}
+
+writeCards(names, event);
+
+let countdown = 0;
+while (countdown < 10){console.log(countdown); countdown++; }
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+
+
+
+
+
